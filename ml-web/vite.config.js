5d02a4d97841e19e4767ff7d9c3c75a860c98032
kellyawaldron <116761579+kellywaldro@users.noreply.github.com>
2025-11-03 14:57:58 -0800

    Update vite.config.js

--- ml-web/vite.config.js
+++ ml-web/vite.config.js
@@ -1,21 +1,13 @@
-// import { defineConfig } from 'vite'
-// import { svelte } from '@sveltejs/vite-plugin-svelte'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [svelte()],
-// })
-
 // ml-web/vite.config.js
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
-// Will live at https://missionlocal.github.io/web/ml-web/
+// Hosted at https://missionlocal.github.io/web/ml-web/
 export default defineConfig({
   plugins: [svelte()],
   base: '/web/ml-web/',
   build: {
-    outDir: '../docs/ml-web',   // publish into the repo's docs folder
+    outDir: './docs/ml-web',  // <-- critical
     emptyOutDir: true
   }
 });
